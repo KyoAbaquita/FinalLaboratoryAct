e5911bf6121918a2ef3e4bcd40dbfa870f5284ab
kyo <kyo@gmail.com>
2025-04-30 12:16:30 +0800

    updated

--- homepage/src/components/About.js
+++ homepage/src/components/About.js
@@ -3,7 +3,7 @@ import React from 'react'
  
 const About = () => {
   return (
-    <div className="text-center text-success mb-4" > 
+    <div className="text-center mb-4" > 
       <h1>About Me</h1>
       <p>Dangal Greetings, I am Abaquita, Shakira Kyo C. 20 yrs old, Currently studying at Pamantasan ng Cabuyao.</p>
     </div>
--- homepage/src/components/Home.js
+++ homepage/src/components/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div className="text-center text-success mb-4"> 
+    <div className="text-center mb-4"> 
       <h1>Welcome to My Page!</h1>
       <p>Hello! I'm Abaquita Shakira Kyo C., a 20-year-old student currently studying at Pamantasan ng Cabuyao, pursuing a Bachelor's degree in Information Technology (BSIT). I'm passionate about technology, programming, and building creative digital solutions.</p>
       <p>As I continue my studies, I strive to enhance my skills and apply what I learn in real-world projects. I'm excited about the future of tech and look forward to exploring new opportunities in software development, web design, and beyond.</p>
