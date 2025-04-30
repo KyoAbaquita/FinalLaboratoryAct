444cab60bedacb67bd5debc8c7e5c4888703f103
kyo <kyo@gmail.com>
2025-04-30 12:06:24 +0800

    updated about

--- homepage/src/components/About.js
+++ homepage/src/components/About.js
@@ -5,8 +5,7 @@ const About = () => {
   return (
     <div> 
       <h1>About Me</h1>
-      <p>Dangal Greetings, I am Abaquita, Shakira Kyo C.</p>
-      <p>Currently studying BSIT</p>
+      <p>Dangal Greetings, I am Abaquita, Shakira Kyo C. 20 yrs old, Currently studying at Pamantasan ng Cabuyao.</p>
     </div>
   )
  
